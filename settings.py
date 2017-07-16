141b59da0dabf569b3b4f3e16961ef21f12c9b44
zacco4 <zacco4@users.noreply.github.com>
2017-07-16 15:16:37 +1000

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,27 @@
+from random import randint
+
+TITLE = "Platform"
+WIDTH = 800
+HEIGHT = 600
+FPS = 60
+
+PLAYER_ACC = 0.8
+PLAYER_FRICTION = -0.12
+PLAYER_GRAV = 0.4
+PLAYER_WIDTH = 40
+PLAYER_HEIGHT = 40
+JUMP_HEIGHT = 15
+
+PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40)]
+
+for i in range(6):
+	PLATFORM_LIST.append((randint(0, WIDTH), randint(0, HEIGHT - 140), randint(40, 100), randint(40, 60)))
+
+
+
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
---
+++ sprites.py
@@ -0,0 +1,52 @@
+import pygame as pg
+from settings import *
+from random import randint
+vec = pg.math.Vector2
+
+class Player(pg.sprite.Sprite):
+	def __init__(self, game):
+		pg.sprite.Sprite.__init__(self)
+		self.game = game
+		self.image = pg.Surface((PLAYER_WIDTH, PLAYER_HEIGHT))
+		self.image.fill(RED)
+		self.rect = self.image.get_rect()
+		self.rect.center = (WIDTH / 2, HEIGHT / 2)
+		self.pos = vec(WIDTH / 2, HEIGHT / 2)
+		self.vel = vec(0, 0)
+		self.acc = vec(0, 0)
+
+	def jump(self):
+		self.rect.x += 1
+		hits = pg.sprite.spritecollide(self, self.game.platforms, False)
+		self.rect.x -= 1
+		if hits:
+			self.vel.y = -JUMP_HEIGHT
+
+	def update(self):
+		self. acc = vec(0, PLAYER_GRAV)
+		keys = pg.key.get_pressed()
+		if keys[pg.K_a]:
+			self.acc.x = -PLAYER_ACC
+		if keys[pg.K_d]:
+			self.acc.x = PLAYER_ACC
+
+		self.acc.x += self.vel.x * PLAYER_FRICTION
+		self.vel += self.acc
+		self.pos += self.vel + 0.5 * self.acc
+		if self.pos.x > WIDTH:
+			self.pos.x = WIDTH - PLAYER_WIDTH
+			self.vel.x = 2
+		if self.pos.x < PLAYER_WIDTH / 2:
+			self.pos.x = PLAYER_WIDTH / 2
+			self.vel.x = 2
+
+		self.rect.midbottom = self.pos
+
+class Platform(pg.sprite.Sprite):
+	def __init__(self, x, y, w, h):
+		pg.sprite.Sprite.__init__(self)
+		self.image = pg.Surface((w, h))
+		self.image.fill((randint(1, 255), randint(1, 255), randint(1, 255)))
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
